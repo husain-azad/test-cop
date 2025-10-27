411f89fd67d4ab51e07cc44beaf2fccdb132d770
husain_azad <husain.azad@cerebital.com>
2025-10-27 12:53:45 +0530

    test3

--- check_function.py
+++ check_function.py
@@ -2,5 +2,6 @@ from test_function import add_numbers, some_numbers
 
 def do_something_with_number(a, b):
     add = add_numbers(a, b)
+    test = some_numbers(a, b)
     result = 5+add
     print(result)
