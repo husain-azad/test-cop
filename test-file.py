960531769d3e0f3e561e7650aa924ad90859483c
husain-azad <husain.azad@cerebital.com>
2025-10-27 13:53:45 +0530

    add error in test-file

--- test-file.py
+++ test-file.py
@@ -6,7 +6,6 @@ from django.db.models import Case, When
 from django.core.cache import cache
 
 from utils.state_manager.mixin import StateManagerMixin
-from system.models import Chart
 from system.api.serializers.chart import ChartSerializer
 from django.db import transaction
 from django.utils.translation import gettext_lazy as _
